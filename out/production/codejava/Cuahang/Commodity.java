67582fbc6b609ed63312bf761909e83cfae1f0bc
baquy2293 <97243640+baquy2293@users.noreply.github.com>
2022-03-07 23:03:49 +0700

    Update and rename commodity.java to Commodity.java

--- out/production/codejava/Cuahang/Commodity.java
+++ out/production/codejava/Cuahang/Commodity.java
@@ -1,27 +1,27 @@
-package Cuahang;
+package Store;
 
 import java.util.Scanner;
 
-public class commodity {
+public class Commoditycommodity {
 
     static void menu() {
         System.out.println("MENU");
-        System.out.println(" 1 : add proproduct");
-        System.out.println(" 2 : remove product");
+        System.out.println(" 1 : Add proproduct");
+        System.out.println(" 2 : Remove product");
         System.out.println(" 3 : Iterrate product");
-        System.out.println(" 4 : search product");
-        System.out.println(" 5 : exit");
+        System.out.println(" 4 : Rearch product");
+        System.out.println(" 5 : Exit");
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Shop newshop = new Shop();
-        product newproduct = new product();
+        Product newProduct = new Product();
 
         while (true) {
             menu();
-            int chon = input.nextInt();
-            switch (chon) {
+            int select = input.nextInt();
+            switch (select) {
                 case 1: {
 
                     newshop.addProduct();
@@ -36,14 +36,14 @@ public class commodity {
                 }
                 case 3: {
 
-                newshop.interProduc();
+                newshop.iterateProduct();
                     break;
                 }
                 case 4: {
                     System.out.println("nhap khoan gia ma ban muo tim");
                     double min = input.nextDouble();
                     double max = input.nextDouble();
-                    newshop.searchproduct(min, max);
+                    newshop.searchProduct(min, max);
                     break;
                 }
                 case 5: {
